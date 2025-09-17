748906c6b233a2937108061cc5ee247df1a18d7e
Charity Nduta <charitynduta29@gmail.com>
2025-09-17 16:29:54 +0300

    Use Card component inside landing page with multiple instances

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,16 @@
-const Landing: React.FC =  () => {
+import Card from "@/components/Card"
+
+const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page</h1>
+    <div className="p-8">
+      <h1 className="text-xl font-extralight mb-6">Landing Page</h1>
+
+      {/* Using Card component */}
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
+
 export default Landing
